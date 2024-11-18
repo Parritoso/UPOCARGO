8928f5f9406092d1a9343349f105105fd5e95bc7
Parritoso <jsanchezclaro@gmail.com>
2024-11-18 23:55:12 +0100

    Añadidos demo data

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,6 @@
     # always loaded
     'data': [
         'security/security.xml',
-        'security/record_rules.xml',
         'security/ir.model.access.csv',
         'views/mudanzas_view.xml',
         'views/clientes_view.xml',
@@ -29,10 +28,20 @@
         'views/bienes_almacenados_view.xml',
         'views/proveedor_view.xml',
         'views/servicios_adicionales_view.xml',
+        'views/empleado_view.xml',
+        'views/factura_view.xml',
+        'views/vehiculo_view.xml',
         'views/menu.xml'
     ],
     # only loaded in demonstration mode
-    'demo': [     
+    'demo': [   
+        'demo/upocargo.almacenamiento.csv',
+        'demo/upocargo.bienes_almacenados.csv',
+        'demo/upocargo.cliente.csv',
+        'demo/upocargo.empleado.csv',
+        'demo/upocargo.proveedor.csv',
+        'demo/upocargo.servicios_adicionales.csv',
+        'demo/upocargo.vehiculo.csv'  
     ],
     'application': True,
 }
